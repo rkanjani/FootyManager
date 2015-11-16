c12d4aa588d6b71238eb9ba5fe1d58e4e71ee96e
rkanjani <rohankanjani1@gmail.com>
2015-11-16 16:28:39 -0500

    fixed

--- app/src/main/java/oceansfive/footymanager/Tournament.java
+++ app/src/main/java/oceansfive/footymanager/Tournament.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 /**
  * Created by rohan on 2015-11-12.
  */
@@ -52,10 +54,6 @@ public class Tournament {
 
         ArrayList<Team> teamList = new ArrayList(); //declares an empty array for list of teams
         boolean started = false;
-    }
-    void addTeam(Team temp){
-        System.out.println("test");
-
     }
 
     public void deleteTeam (Team delTeam){
