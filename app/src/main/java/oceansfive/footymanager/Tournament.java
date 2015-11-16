471288c44a88880237cc3346dee8d4525a7b695d
Andrew Delph <andrewdelph@live.ca>
2015-11-15 23:48:09 -0500

    added class variables to Tournament
    teamlist and addteam

--- app/src/main/java/oceansfive/footymanager/Tournament.java
+++ app/src/main/java/oceansfive/footymanager/Tournament.java
@@ -1,8 +1,16 @@
 package oceansfive.footymanager;
 
+import java.util.ArrayList;
+
 /**
  * Created by rohan on 2015-11-12.
  */
 public class Tournament {
+    ArrayList<Team> teamList=new ArrayList(); //declares an empty array for list of teams
+    boolean started= false;
+
+    void addTeam(Team temp){
+        System.out.println("test");
+    }
 
 }
