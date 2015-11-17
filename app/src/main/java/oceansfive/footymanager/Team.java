38a0830a7372dbea8cffcfc709cfbae4e9e61037
tmetade <tmetade@yahoo.ca>
2015-11-16 19:41:00 -0500

    added some methods to Team class

--- app/src/main/java/oceansfive/footymanager/Team.java
+++ app/src/main/java/oceansfive/footymanager/Team.java
@@ -7,11 +7,38 @@ import java.util.List;
  */
 public class Team {
     private String teamName;
-    private Player[] players = new Player[28];
+    private List<Player> players = new ArrayList<Player>();
+
+    //keeps track of the size of the team.
+    private int teamSize = 0;
 
     public Team(String teamName)
     {
         this.teamName = teamName;
+    }
+
+    public String getTeamName(){
+        return teamName;
+    }
+
+    //adds player to the team
+    public void addPlayer(String name, char position, int jerseyNumber){
+        if(teamSize<=28) {
+            Player player1 = new Player(name, position, jerseyNumber);
+            players.add(player1);
+            teamSize++;
+        }
+        System.out.println("no more splace on the roster");
+    }
+
+    //deletes player
+    public void deletePlayer(Player guy){
+        players.remove(guy);
+    }
+
+    //edits the players posiiton
+    public void editPlayerPosition(Player p1, char pos){
+        p1.setPosition(pos);
     }
 
 }
