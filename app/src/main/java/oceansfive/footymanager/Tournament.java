ff64e1a3ed9612e59bfa45e7b866957cba10d088
rkanjani <rohankanjani1@gmail.com>
2015-11-17 14:13:01 -0500

    TournamentData Cleanup

--- app/src/main/java/oceansfive/footymanager/Tournament.java
+++ app/src/main/java/oceansfive/footymanager/Tournament.java
@@ -11,15 +11,26 @@ import java.util.ArrayList;
  * Created by rohan on 2015-11-12.
  */
 public class Tournament {
+<<<<<<< Updated upstream
 
     private String tournamentName;
     private String tournamentType;
+=======
+    boolean started= false;
+
+    String tournamentName;
+    protected String tournamentType;
+    String tournamentLogo;
+    int tournamentSize;
+>>>>>>> Stashed changes
     List<Team> teams = new ArrayList<Team>();
     List<Game> games = new ArrayList<Game>();
 
-    public void Tournament(String tournamentName, String tournamentType){
-        this.tournamentName = tournamentType;
-        this.tournamentType = tournamentName;
+    public Tournament(String tournamentName, String tournamentType, int tournamentSize, String tournamentLogo){
+        this.tournamentName = tournamentName;
+        this.tournamentType = tournamentType;
+        this.tournamentLogo = tournamentLogo;
+        this.tournamentSize = tournamentSize;
     }
 
     public String getTournamentName(){
@@ -29,6 +40,12 @@ public class Tournament {
     public String getTournamentType() {
         return tournamentType;
     }
+    public int getSize(){
+        return tournamentSize;
+    }
+    public String getLogo(){
+        return tournamentLogo;
+    }
     public List<Team> getTeams(){
         return teams;
     }
@@ -36,7 +53,6 @@ public class Tournament {
     public List<Game> getGames(){
         return games;
     }
-
     public void createGames(List teams){
         Collections.shuffle(teams);
         Team[] teamArr = new Team[teams.size()];
@@ -52,11 +68,14 @@ public class Tournament {
 
     }
     public void addTeam(String teamName) {
-        Team createTeam = new Team(teamName);
-        teams.add(createTeam);
+        Team createdTeam = new Team(teamName);
+        teams.add(createdTeam);
     }
-
     public void deleteTeam (Team delTeam){
         teams.remove(delTeam);
+    }
+
+    public String toString(){
+        return tournamentName;
     }
 }
