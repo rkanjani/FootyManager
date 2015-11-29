ee84206d6f6e1cc4bc764544f19cf9f03b22213e
onomtam <matt.tam25@gmail.com>
2015-11-29 15:15:06 -0500

    Schedule List Adapter

--- app/src/main/java/oceansfive/footymanager/ScheduleListAdapter.java
+++ app/src/main/java/oceansfive/footymanager/ScheduleListAdapter.java
@@ -8,65 +8,56 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
-import java.util.List;
-
 /**
- * Created by Matthew on 11/29/2015.
+ * Created by Matt on 2015-11-29.
  */
 public class ScheduleListAdapter extends BaseAdapter {
 
-    Context context;
     private static LayoutInflater inflater = null;
 
-    //***Later we should pass an array of tournaments instead of 4 Lists****//
-    List<Team> teams;
+    Context context;
+    TournamentData data = TournamentData.getInstance();
+    Tournament tournament;
+    Team[] standings;
 
-    public ScheduleListAdapter(Context context, Tournament Tournaments) {
+    public ScheduleListAdapter(Context context, Tournament tournament) {
         // TODO Auto-generated constructor stub
         this.context = context;
-        this.teams = teams;
+        this.tournament = tournament;
         inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-    @Override
-    public int getCount() {
-        // TODO Auto-generated method stub
-        return teams.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        // TODO Auto-generated method stub
-        return teams.get(position);
-    }
-    @Override
-    public long getItemId(int position) {
-        // TODO Auto-generated method stub
-        return position;
+        //this.standings = tournament.getRanking();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
         View vi = convertView;
         if (vi == null)
             vi = inflater.inflate(R.layout.tournament_row_item, null);
-        TextView name = (TextView) vi.findViewById(R.id.tournamentName);
-        TextView size = (TextView) vi.findViewById(R.id.tournamentSize);
-        TextView type = (TextView) vi.findViewById(R.id.tournamentType);
-
-        ImageView logo = (ImageView) vi.findViewById(R.id.tournamentLogo);
-        String uri = teams.get(position).getTeamLogo();
+        ImageView team1 = (ImageView) vi.findViewById(R.id.teamLogo);
+        ImageView team2 = (ImageView) vi.findViewById(R.id.teamLogo);
+        ImageView teamWin = (ImageView) vi.findViewById(R.id.teamLogo);
 
 
-        name.setText(teams.get(position).getTeamName());
-//        size.setText("Number of teams: " + teams.get(position).getTournamentSize());
-//        type.setText(teams.get(position).getTeamName());
-        logo.setImageResource(this.context.getResources().getIdentifier(uri, "drawable", this.context.getPackageName()));
+        team1.setImageResource(this.context.getResources().getIdentifier(uri, "drawable", this.context.getPackageName()));
+        team2.setImageResource(this.context.getResources().getIdentifier(uri, "drawable", this.context.getPackageName()));
+        teamWin.setImageResource(this.context.getResources().getIdentifier(uri, "drawable", this.context.getPackageName()));
 
         return vi;
     }
 
+    @Override
+    public int getCount() {
+
+        return standings.length;
+    }
+
+    @Override
+    public Team getItem(int position) {
+        return standings[position];
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
