560683162955b28d32e87c74ef540d702a86508b
tmetade <tmetade@yahoo.ca>
2015-11-29 14:40:18 -0500

    added set stuff to team info

--- app/src/main/java/oceansfive/footymanager/TeamInfo.java
+++ app/src/main/java/oceansfive/footymanager/TeamInfo.java
@@ -5,13 +5,31 @@ import android.os.Bundle;
 import android.widget.*;
 
 public class TeamInfo extends AppCompatActivity {
+    final Team team = data.tournaments.get(getIntent().getExtras().getInt("team"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_info);
 
-        TextView text = (TextView) findViewById(R.id.teamName);
-        text.setText("your text!");
+        EditText teamName = (EditText) findViewById(R.id.teamName);
+        teamName.setText(team.getTeamName());
+
+        ImageView img= (ImageView) findViewById(R.id.image);
+        img.setImageURI();
+
+        TextView wins = (TextView) findViewById(R.id.numOfWin);
+        wins.setText(team.getWins());
+
+        TextView losses = (TextView) findViewById(R.id.numOfLoss);
+        losses.setText(team.getLosses());
+
+        TextView red = (TextView) findViewById(R.id.numOfRed);
+        red.setText(team.getRedCards());
+
+        TextView yellow = (TextView) findViewById(R.id.numOfWin);
+        yellow.setText(team.getYellowCards());
+
+
     }
 }
