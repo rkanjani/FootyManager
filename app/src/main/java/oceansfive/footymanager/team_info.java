b4ba4b9a23cecefd3492e9d9ec1fe7034b47531f
tmetade <tmetade@yahoo.ca>
2015-11-29 12:23:10 -0500

    Changed team screen and enter score to default values

--- app/src/main/java/oceansfive/footymanager/team_info.java
+++ app/src/main/java/oceansfive/footymanager/team_info.java
@@ -2,6 +2,7 @@ package oceansfive.footymanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class team_info extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class team_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_info);
+
+        TextView text = (TextView) findViewById(R.id.teamName);
+        text.setText("your text!");
     }
 }
