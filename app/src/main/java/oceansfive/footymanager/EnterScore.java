7047a545c4abf0fe711ac6422d3e8789dfc39286
rkanjani <rohankanjani1@gmail.com>
2015-11-29 17:35:45 -0500

    Added new parameter to tournament constructor

--- app/src/main/java/oceansfive/footymanager/EnterScore.java
+++ app/src/main/java/oceansfive/footymanager/EnterScore.java
@@ -6,7 +6,7 @@ import android.widget.*;
 
 public class EnterScore extends AppCompatActivity {
     TournamentData data  = TournamentData.getInstance();
-    final Game game = data.tournaments.get(getIntent().getExtras().getInt("tournament")).games[getIntent().getExtras().getInt("game")];
+    final Game game = data.tournaments.get(getIntent().getExtras().getInt("tournament")).games.get(getIntent().getExtras().getInt("game"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
