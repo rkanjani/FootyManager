e3b184e61a30438a41b9dc1dc4c78246751056a3
rkanjani <rohankanjani1@gmail.com>
2015-12-02 23:24:48 -0500

    Updated winner page

--- app/src/main/java/oceansfive/footymanager/winner.java
+++ app/src/main/java/oceansfive/footymanager/winner.java
@@ -1,7 +1,10 @@
 package oceansfive.footymanager;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.*;
 
 public class winner extends AppCompatActivity {
@@ -11,6 +14,7 @@ public class winner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
         setTitle("Winner!");
+        getSupportActionBar().hide();
 
         String [] info = getIntent().getStringArrayExtra("info");
 
@@ -24,6 +28,12 @@ public class winner extends AppCompatActivity {
 
         TextView numLoss = (TextView) findViewById(R.id.winnerLoss);
         numLoss.setText(info[2]);
+
+    }
+    public void onBackPressed(){
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent, 0);
 
     }
 }
