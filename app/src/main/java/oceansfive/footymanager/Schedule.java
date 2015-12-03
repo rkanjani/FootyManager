38002319b3f10bb0a43c12cb6e8c85506ae84995
rkanjani <rohankanjani1@gmail.com>
2015-12-02 22:42:03 -0500

    Added button state change

--- app/src/main/java/oceansfive/footymanager/Schedule.java
+++ app/src/main/java/oceansfive/footymanager/Schedule.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -33,6 +34,9 @@ public class Schedule extends AppCompatActivity {
 
 
         tournament = data.tournaments.get(tournamentIndex);
+
+        if(tournament.getTournamentType().equals("Round Robin"))
+            ((Button)(findViewById(R.id.updateRound))).setText("Complete Tournament");
 
 
         setTitle("League Schedule");
@@ -102,7 +106,7 @@ public class Schedule extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), winner.class);
                 intent.putExtra("info", info);
-                startActivityForResult(intent,0);
+                startActivityForResult(intent, 0);
                 return;
             }
            // adapter.notifyDataSetChanged();
